02878dfc93f4bb16b08faaabdc71eb64c2ac195b
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-11-15 02:06:15 +0100

    -

---
+++ 06-tools.c
@@ -0,0 +1,68 @@
+#include "shell.h"
+
+
+void free2darr(char **arr)
+{
+    int i;
+    if (!arr)
+        return;
+    for (i = 0; arr[i]; i++)
+    {
+        free(arr[i]); 
+        arr[i] = NULL;
+    }
+        free(arr);
+        arr = NULL;
+}
+
+void printerror(char *name, char *cmd, int idx)
+{
+    char *index, mssg[] =": not found\n";
+    index = _itoa(idx);
+
+    write(STDERR_FILENO, name, _strlen(name));
+    write(STDERR_FILENO, ": ", 2);
+    write(STDERR_FILENO, index, _strlen(index));
+    write(STDERR_FILENO, ": ", 2);
+    write(STDERR_FILENO, cmd, _strlen(cmd));
+    write(STDERR_FILENO, mssg, _strlen(mssg));
+    free(index);
+}
+
+char *_itoa(int n)
+{
+    char buffer[20];
+    int i =0;
+
+    if (n ==0)
+        buffer[i++] = '0';
+    else
+    {
+        while (n>0)
+        {
+            buffer[i++] = (n% 10) +'0';
+            n /= 10;
+        }
+    }
+
+    buffer[i] = '\0';
+    reverse_strings(buffer, i);
+    return(_strdup(buffer));    
+}
+
+void reverse_strings(char *str, int len)
+{
+    char tmp;
+    int start =0;
+    int end = len -1;
+
+    while (start < end)
+    {
+        tmp = str[start];
+        str[start] = str[end];
+        str[end] = tmp;
+        start++;
+        end --;
+    }
+    
+}
